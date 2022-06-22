2af753dd10bd15d0f4c10a0eeecb59abca9f811a
Atai Omorov <78410415+atainaive@users.noreply.github.com>
2022-06-22 10:00:43 +0300

    Update FirstApplication.java

--- src/main/java/com/mjc/stage0/FirstApplication.java
+++ src/main/java/com/mjc/stage0/FirstApplication.java
@@ -2,6 +2,6 @@ package com.mjc.stage0;
 
 public class FirstApplication {
     public static void main(String[] args) {
-        System.out.prinln("Hello, World!");
+        System.out.println("Hello, World!");
     }
 }
